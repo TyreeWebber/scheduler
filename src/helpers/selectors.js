d43de3217bb13e2d5dd1717ff8d9a19ce08d6078
Tyree Webber <webber.tyree@gmail.com>
2022-07-14 15:31:47 -0600

    create helpers/selectors and selectors.test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  let newArr = [];
+  for (let dayObject of state.days) {
+    if (day === dayObject.name) {
+      for (let app of dayObject.appointments) {
+        newArr.push(state.appointments[app]);
+      }
+    }
+  }
